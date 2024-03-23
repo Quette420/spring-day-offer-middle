1ef9e43019f2e21e230f1a7e01e85daad90416f8
George <gsaranov@gmail.com>
2024-03-23 12:04:13 +0300

    refactoring

--- src/main/java/com/onedayoffer/taskdistribution/services/EmployeeService.java
+++ src/main/java/com/onedayoffer/taskdistribution/services/EmployeeService.java
@@ -6,66 +6,67 @@ import com.onedayoffer.taskdistribution.DTO.TaskStatus;
 import com.onedayoffer.taskdistribution.exceptions.TaskNotFoundException;
 import com.onedayoffer.taskdistribution.exceptions.UserNotFoundException;
 import com.onedayoffer.taskdistribution.repositories.EmployeeRepository;
-import com.onedayoffer.taskdistribution.repositories.TaskRepository;
 import com.onedayoffer.taskdistribution.repositories.entities.Employee;
 import com.onedayoffer.taskdistribution.repositories.entities.Task;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Sort;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
-    private final TaskRepository taskRepository;
     private final ModelMapper modelMapper;
 
     public List<EmployeeDTO> getEmployees(@Nullable String sortDirection) {
-        Type listType = new TypeToken<List<EmployeeDTO>>() {}.getType();
+        log.info("getEmployees({})", sortDirection);
         List<Employee> employees;
         if (Optional.ofNullable(sortDirection).isPresent()) {
             Sort.Direction direction = Sort.Direction.valueOf(sortDirection);
             employees = employeeRepository.findAllAndSort(Sort.by(direction, "fio"));
-            return modelMapper.map(employees, listType);
         } else {
             employees = employeeRepository.findAll();
-            return modelMapper.map(employees, listType);
         }
+        return modelMapper.map(employees, new TypeToken<List<EmployeeDTO>>() {}.getType());
     }
 
     @Transactional
     public EmployeeDTO getOneEmployee(Integer id) {
-        Employee employee = getEmployeeByIdIfPresent(id);
+        log.info("getOneEmployee({})", id);
+        var employee = getEmployeeByIdIfPresent(id);
         return modelMapper.map(employee, EmployeeDTO.class);
     }
 
     public List<TaskDTO> getTasksByEmployeeId(Integer id) {
-        Employee employee = getEmployeeByIdIfPresent(id);
+        log.info("getTasksByEmployeeId({})", id);
+        var employee = getEmployeeByIdIfPresent(id);
         return employee.getTasks().stream().map(task -> modelMapper.map(task, TaskDTO.class)).collect(Collectors.toList());
     }
 
     @Transactional
     public void changeTaskStatus(Integer employeeId, Integer taskId, TaskStatus status) {
-        Employee employee = getEmployeeByIdIfPresent(employeeId);
-        Task task = employee.getTasks().stream().filter(task1 -> task1.getId().equals(taskId)).findFirst().orElseThrow(() -> new TaskNotFoundException("Task with id " + taskId + " not found"));
+        log.info("changeTaskStatus(employeeId = {}, taskId = {}, status = {})", employeeId, taskId, status);
+        var employee = getEmployeeByIdIfPresent(employeeId);
+        var task = employee.getTasks().stream().filter(task1 -> task1.getId().equals(taskId)).findFirst().orElseThrow(() -> new TaskNotFoundException("Task with id " + taskId + " not found"));
         task.setStatus(status);
     }
 
     @Transactional
     public void postNewTask(Integer employeeId, TaskDTO newTask) {
-        Employee employee = getEmployeeByIdIfPresent(employeeId);
-        Task task = modelMapper.map(newTask, Task.class);
+        log.info("changeTaskStatus(employeeId = {}, newTask = {})", employeeId, newTask);
+        var employee = getEmployeeByIdIfPresent(employeeId);
+        var task = modelMapper.map(newTask, Task.class);
         employee.addTask(task);
     }
 
